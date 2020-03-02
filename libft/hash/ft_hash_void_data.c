2211441d5a93da40a2296b17d7ab36ebe7a25dc0
Francis KANTE <fkante@e2r11p13.42.fr>
2020-03-02 10:10:22 +0100

    [master] hash function

--- libft/hash/ft_hash_void_data.c
+++ libft/hash/ft_hash_void_data.c
@@ -6,7 +6,7 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 10:25:03 by fkante            #+#    #+#             */
-/*   Updated: 2020/02/28 11:49:05 by fkante           ###   ########.fr       */
+/*   Updated: 2020/03/02 10:07:03 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,12 @@
 ** stackoverflow.com/questions/10696223/reason-for-5381-number-in-djb-hash-function
 */
 
-uint32_t	ft_hash_void_data(void *data, uint32_t *previous_hash)
+uint32_t	ft_hash_void_data(void *data)
 {
 	t_vector	*vct_data;
 	uint32_t	hash_val;
 	size_t		i;
 
-	(void)previous_hash;
 	vct_data = vct_newstr((char*)data);
 	hash_val = DJB_HASH_CONSTANT;
 	i = 0;
