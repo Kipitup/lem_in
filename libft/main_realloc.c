367be28b2c7b9bd15600bdb15da5c1e1c9e65122
Francis KANTE <fkante@e2r11p13.42.fr>
2020-03-04 09:51:28 +0100

    [REALLOC] small fix

--- libft/main_realloc.c
+++ libft/main_realloc.c
@@ -6,7 +6,7 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 11:38:56 by fkante            #+#    #+#             */
-/*   Updated: 2020/03/03 13:26:00 by fkante           ###   ########.fr       */
+/*   Updated: 2020/03/04 09:49:47 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 int		main(void)
 {
-	char	test[] = "salut ca va ?";
+	char	test[] = "cafait8";
 	void	*content;
 	char	*new_content;
+	int		size = 9;
 
 	content = ft_memalloc(ft_strlen(test) + 1);
 	ft_strcpy((char *)content, test);
-	new_content = (char*)ft_realloc(content, 5);
-	for (int i = 0; i < 4; i++)
+	new_content = (char*)ft_realloc(content, size);
+	for (int i = 0; i < size; i++)
 		ft_printf("new_content = %c\n", new_content[i]++);
 	return (0);
 }
--- libft/memory/ft_realloc.c
+++ libft/memory/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: fkante <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 11:25:17 by fkante            #+#    #+#             */
-/*   Updated: 2020/03/03 13:02:22 by fkante           ###   ########.fr       */
+/*   Updated: 2020/03/04 09:49:47 by fkante           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,22 @@
 void	*ft_realloc(void *content, size_t size)
 {
 	void	*tmp;
-	
-	if (size > 0)
+
+	tmp = NULL;
+	if (content != NULL)
 	{
-		tmp = ft_memalloc(size);
-		if (tmp != NULL)
+		if (size > 0)
 		{
-			ft_memcpy(tmp, content, size);
-			ft_memdel(&content);
-			return (tmp);
+			tmp = ft_memalloc(size);
+			if (tmp != NULL)
+			{
+				ft_memcpy(tmp, content, size);
+				ft_memdel(&content);
+				return (tmp);
+			}
 		}
 	}
-	return (NULL);
+	else
+		ft_print_err_null("realloc failed / content is NULL", STD_ERR);
+	return (tmp);
 }
