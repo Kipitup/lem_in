3605a064ed85993bcc10fd7272ba005adb6bbded
amartino <a.martinod1@gmail.com>
2020-03-30 16:32:27 +0200

    remove some typecast in libft and rename the fiel set_or_rules.txt

--- libft/stats/ft_get_median.c
+++ libft/stats/ft_get_median.c
@@ -6,7 +6,7 @@
 /*   By: amartino <amartino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/30 15:49:05 by amartino          #+#    #+#             */
-/*   Updated: 2020/03/12 14:38:19 by amartino         ###   ########.fr       */
+/*   Updated: 2020/03/29 16:29:14 by amartinod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 ssize_t		ft_get_median(int32_t *tab, size_t size)
 {
+	size_t		i;
 	int32_t		median;
-	ssize_t		i;
 	int8_t		ret;
 
 	i = 0;
