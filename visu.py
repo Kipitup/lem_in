3729803285da5c9e3d64a75548d25a68c18b3022
kipit <kipit@42l.fr>
2020-04-22 15:46:19 +0200

    [develop] visu V1.1 add color for first and last node

--- visu.py
+++ visu.py
@@ -6,7 +6,7 @@
 #    By: amartinod <amartino@student.42.fr>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/04/22 11:00:06 by amartinod         #+#    #+#              #
-#    Updated: 2020/04/22 15:00:03 by amartinod        ###   ########.fr        #
+#    Updated: 2020/04/22 15:45:36 by amartinod        ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,8 +21,24 @@ with open(filepath) as fp:
    while line:
        line = line.rstrip('\n')
        data = line.split('-') #split string into a list
-       G.add_edge(data[0], data[1], color='red')
+       G.add_edge(int(data[0]), int(data[1]), color='red')
        line = fp.readline()
 
-nx.draw(G, with_labels=True, font_weight='bold')
+pos=nx.spring_layout(G) # positions for all nodes
+
+#nx.draw(G, with_labels=True, font_weight='bold')
+
+nodes = list(G.nodes)
+firstNode = nodes.pop(0)
+lastNode = nodes.pop() # If no index is specified, removes and returns the last item in the list.
+
+nx.draw_networkx_nodes(G, pos, nodes, node_color='b')
+nx.draw_networkx_nodes(G, pos, nodelist=[firstNode], node_color='g')
+nx.draw_networkx_nodes(G, pos, nodelist=[lastNode], node_color='r')
+
+nx.draw_networkx_edges(G, pos, list(G.edges))
+
+nx.draw_networkx_labels(G, pos, font_size=10)
+
+plt.axis('off')
 plt.show()
