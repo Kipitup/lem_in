3b891118623e3b37973a010770be069379488a7d
idinorio <francis.kante@gmail.com>
2020-05-11 10:16:22 +0200

    [master] norme on lem_in.c

--- srcs/solve/lem_in.c
+++ srcs/solve/lem_in.c
@@ -6,18 +6,36 @@
 /*   By: amartino <amartino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 16:47:02 by amartino          #+#    #+#             */
-/*   Updated: 2020/05/11 09:55:54 by francis          ###   ########.fr       */
+/*   Updated: 2020/05/11 10:16:09 by francis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+static uint8_t	store_path_handle_link(t_solution *sol, t_lemin *lemin,
+		size_t used_multiple)
+{
+	uint8_t	ret;
+
+	ret = FALSE;
+	if ((store_valid_path_and_reset(sol, &used_multiple)) == FAILURE)
+	{
+		if (smart_ant_management(sol, lemin->nb_ants) == TRUE)
+			ret = TRUE;
+		else
+			handle_link_used_both_way(lemin);
+	}
+	return (ret);
+}
+
 void			lem_in(t_lemin *lemin)
 {
 	t_solution	*sol;
+	size_t		used_multiple;
 	int8_t		ret;
 
 	ret = SUCCESS;
+	used_multiple = 0;
 	if (lemin != NULL && lemin->result != NULL)
 	{
 		while (ret == SUCCESS)
@@ -26,13 +44,8 @@ void			lem_in(t_lemin *lemin)
 			ret = bfs(sol);
 			if (ret == SUCCESS)
 			{
-				if ((store_valid_path_and_reset(sol, &used_multiple)) == FAILURE)
-				{
-					if (smart_ant_management(sol, lemin->nb_ants) == TRUE)
-						break ;
-					else
-						handle_link_used_both_way(lemin);
-				}
+				if (store_path_handle_link(sol, lemin, used_multiple) == TRUE)
+					break ;
 			}
 			else
 				smart_ant_management(sol, lemin->nb_ants);
